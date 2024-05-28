39aa0d788a1ad5b800fad5bd6b61f595b991de3a
pkrnganesh <144223747+pkrnganesh@users.noreply.github.com>
2024-05-29 01:32:53 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { ListFormat } from "typescript";
+ReactDom.render(
+  <div>
+    <h1>hi</h1>
+    <ul>
+      <li>this is ram </li>
+      <li>iam an aeronautical engineer</li>
+      <li>but studying cs</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
